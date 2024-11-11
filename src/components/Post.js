4313fc9d9d18d4863080ba6bbcc91f3da87c51ab
sroballos <sroballos@udesa.edu.ar>
2024-11-11 18:34:08 -0300

    Avance en uso de datos de firebase

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,21 @@
 import { Component } from 'react'
-import { Text } from 'react-native-web'
+import { Text, View, StyleSheet } from 'react-native-web'
 
 export default class Post extends Component {
   render() {
     return (
+      <View style ={styles.container}>
       <Text>Post</Text>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- src/screens/HomeMenu.js
+++ src/screens/HomeMenu.js
@@ -1,12 +1,43 @@
-import { Component } from 'react'
-import { Text, View } from 'react-native-web'
+import { Component } from "react";
+import { Text, View, FlatList } from "react-native-web";
+import Post from "../components/Post";
+import { auth, db } from "../firebase/config";
 
 export default class HomeMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    db.collection("posts").onSnapshot((docs) => {
+      let posts = [];
+      docs.forEach((doc) => {
+        posts.push({ id: doc.id, data: doc.data() });
+      });
+      console.log(posts);
+      this.setState({
+        posts: posts,
+      });
+    });
+  }
+
   render() {
     return (
       <View>
         <Text>Home</Text>
+        <Post />
+        {this.state.posts.length != 0 && (
+          <FlatList
+            data={this.state.posts}
+            keyExtractor={(post) => post.id}
+            renderItem={({ item }) => <Text>{item.data.msg}</Text>}
+          />
+        )}
       </View>
-    )
+    );
   }
 }
