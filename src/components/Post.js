d8ca1fb4c779ee9e97b18b1bcf8a8f12d776aafe
sroballos <sroballos@udesa.edu.ar>
2024-11-12 14:02:31 -0300

    Posts, css

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,10 +2,20 @@ import { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native-web'
 
 export default class Post extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: this.props.userName,
+      content: this.props.content,
+      likes: this.props.likes
+    }
+  }
   render() {
     return (
       <View style ={styles.container}>
-      <Text>Post</Text>
+      <Text style={styles.mainText}>{this.state.content}</Text>
+      <Text>Por {this.state.userName}</Text>
+      <Text>Likes: {this.state.likes}</Text>
       </View>
     )
   }
@@ -13,9 +23,17 @@ export default class Post extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "red",
+    width: "30%",
+    height: "30%",
+    margin: "10%",
+    backgroundColor: "#22c6e2",
     alignItems: "center",
     justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 15,
   },
+  mainText: {
+    fontSize: 30,
+  }
 });
--- src/screens/HomeMenu.js
+++ src/screens/HomeMenu.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Text, View, FlatList } from "react-native-web";
+import { Text, View, FlatList, StyleSheet } from "react-native-web";
 import Post from "../components/Post";
 import { auth, db } from "../firebase/config";
 
@@ -27,17 +27,32 @@ export default class HomeMenu extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Home</Text>
-        <Post />
+        <View style={styles.postContainer}>
         {this.state.posts.length != 0 && (
           <FlatList
             data={this.state.posts}
             keyExtractor={(post) => post.id}
-            renderItem={({ item }) => <Text>{item.data.msg}</Text>}
+            renderItem={({ item }) => <Post content={item.data.msg} userName={item.data.user} likes={`${item.data.likes}`} />}
           />
         )}
+        </View>
+
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  postContainer: {
+    flex: 1,
+    flexDirection: "Row",
+    width:"90%"
+  }
+});
